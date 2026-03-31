52029de60faa90b9e8708a25c3cc6fcd778952c3
kecapbautroi <116443446+Vietduy97@users.noreply.github.com>
2026-03-31 21:53:21 +0700

    sua lai vao package Common

---
+++ src/main/java/Common/Admin.java
@@ -0,0 +1,22 @@
+package Common;
+
+public class Admin extends User {
+
+    public Admin(String username, String password, String fullName) {
+        super(username, password, fullName, "ADMIN");
+    }
+
+    public void banUser(User user) {
+        System.out.println("Admin banned user: " + user.getUsername());
+    }
+
+    public void viewAllUsers() {
+        System.out.println("Admin is viewing all users...");
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Role: Admin");
+    }
+}
---
+++ src/main/java/Common/Bidder.java
@@ -0,0 +1,34 @@
+package Common;
+
+public class Bidder extends User {
+
+    private double balance;
+
+    public Bidder(String username, String password, String fullName, double balance) {
+        super(username, password, fullName, "BIDDER"); // set role cứng
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public boolean placeBid(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Balance: " + balance);
+    }
+}
