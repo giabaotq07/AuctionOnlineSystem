4896c1ab3c46b7d108d2c303a7d059fabca1b695
kecapbautroi <116443446+Vietduy97@users.noreply.github.com>
2026-03-31 21:29:56 +0700

    firstpushcommon
    common 1 so thu co ban ve item

---
+++ BTL/src/User.java
@@ -0,0 +1,21 @@
+public abstract class User extends Entity {
+    private String username;
+    private String password;
+    private String fullName;
+    private String role; // Có thể dùng Enum: BIDDER, SELLER, ADMIN [cite: 34]
+
+    public User(String username, String password, String fullName, String role) {
+        super();// Gọi constructor của Entity
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+    @Override
+    public void printInfo() {
+        System.out.println("User ID: " + id + " | Username: " + username + " | Role: " + role);
+    }
+    // Getters & Setters
+    public String getUsername() { return username; }
+    public String getRole() { return role; }
+}
