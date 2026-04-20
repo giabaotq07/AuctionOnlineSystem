30ca28af1f9d0f42b2716b22d3248e235f9fccab
giabaotq07 <email_cua_ban@example.com>
2026-04-20 22:10:59 +0700

    Them Admin va Seller

---
+++ src/main/java/Common/Seller.java
@@ -0,0 +1,17 @@
+package Common;
+
+public class Seller extends User implements AuctionObserver {
+    public Seller(String id, String username) {
+        super(id, username);
+    }
+
+    @Override
+    public void onNewBidPlaced(String itemName, double newPrice, String bidderName) {
+        System.out.println("[" + getName() + "]: Thông báo! '" + bidderName + "' vừa trả giá $" + newPrice + " cho mặt hàng của bạn: " + itemName);
+    }
+
+    @Override
+    public void onAuctionClosed(String itemName, String winnerName, double finalPrice) {
+        System.out.println("[" + getName() + "]: Phiên đấu giá cho mặt hàng của bạn '" + itemName + "' đã kết thúc. Người thắng: " + winnerName + " với giá $" + finalPrice);
+    }
+}
