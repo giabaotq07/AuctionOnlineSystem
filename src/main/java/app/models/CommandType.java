9e6ba79bb570578bb24b141aac9583b3e136fbda
Lazuly <nhlazuly@gmail.com>
2026-04-22 17:40:44 +0700

    thêm trạng thái

--- src/main/java/app/models/CommandType.java
+++ src/main/java/app/models/CommandType.java
@@ -10,6 +10,12 @@ public enum CommandType {
   PLACE_BID, // Client đặt giá
   CREATE_AUCTION, // Seller tạo phiên
   CHAT,
+  //status
+  OPEN,
+  RUNNING,
+  FINISHED,
+  PAID,
+  CANCELED,
 
   // Server Push (Realtime)
   UPDATE_PRICE, // Notify giá mới cho tất cả Client
