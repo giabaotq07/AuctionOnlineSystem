fed7101664fac53c07c008953e417b6fef383583
Lazuly <nhlazuly@gmail.com>
2026-04-24 18:38:25 +0700

    checkstyle

--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -81,12 +81,12 @@ public class User {
   }
 
   @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
-        User user = (User) o;
+    User user = (User) o;
 
-        return account.equals(user.account) && name.equals(user.name);
-    }
+    return account.equals(user.account) && name.equals(user.name);
+  }
 }
--- src/test/java/app/models/UserDaoTest.java
+++ src/test/java/app/models/UserDaoTest.java
@@ -1,33 +1,32 @@
 package app.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import app.config.DatabaseConnection;
 import app.dao.UserDAO;
+import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class UserDaoTest {
-    UserDAO userDAO;
-    User user0;
-    @BeforeEach
-    void setUp() {
-        try {
-            DatabaseConnection.getConnection();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        userDAO = new UserDAO();
-        userDAO.deleteUser("test_account");
-        user0 = userDAO.addUser("test_account", "test_password", "Test User");
+  UserDAO userDAO;
+  User user0;
 
+  @BeforeEach
+  void setUp() {
+    try {
+      DatabaseConnection.getConnection();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    userDAO = new UserDAO();
+    userDAO.deleteUser("test_account");
+    user0 = userDAO.addUser("test_account", "test_password", "Test User");
+  }
 
-    }
-    @Test
-    void testLoadUser() {
-        User user1 = userDAO.loadUsers("test_account");
-        assertEquals(user1, user0);
-    }
+  @Test
+  void testLoadUser() {
+    User user1 = userDAO.loadUsers("test_account");
+    assertEquals(user1, user0);
+  }
 }
