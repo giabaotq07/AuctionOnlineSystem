7016c564a1dd5edbd28e9ab172d93c4d4f6254f0
Lazuly <nhlazuly@gmail.com>
2026-05-19 09:44:47 +0700

    sửa cấu trúc rõ hơn

--- common/src/main/java/app/common/enums/AuctionStatus.java
+++ common/src/main/java/app/common/enums/AuctionStatus.java
@@ -2,20 +2,10 @@ package app.common.enums;
 
 /** AuctionStatus. */
 public enum AuctionStatus {
-  OPEN(0),
-  RUNNING(1),
-  ENDING_SOON(2),
-  FINISHED(3),
-  PAID(4),
-  CANCELED(5),
-  ;
-  private final int state;
-
-  AuctionStatus(int state) {
-    this.state = state;
-  }
-
-  public int getState() {
-    return state;
-  }
+  OPEN,
+  RUNNING,
+  ENDING_SOON,
+  FINISHED,
+  PAID,
+  CANCELED,
 }
