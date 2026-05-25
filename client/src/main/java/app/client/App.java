9b6106718680874c73fd3932b00c76c2c2549042
Lazuly <nhlazuly@gmail.com>
2026-05-25 11:01:27 +0700

    sửa ui

--- client/src/main/java/app/client/App.java
+++ client/src/main/java/app/client/App.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class App {
   private static final Logger logger = LoggerFactory.getLogger(App.class);
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     Application.launch(Main.class, args);
     Runtime.getRuntime()
         .addShutdownHook(
